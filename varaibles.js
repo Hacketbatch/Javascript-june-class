538e04d2f02a105257bc022315229b9811455e32
Albert Kipchirchir <albertkipchirchir02@gmail.com>
2025-07-09 12:03:17 +0300

    js intro and variables

---
+++ varaibles.js
@@ -0,0 +1,24 @@
+// Varaibles are used to store data that can be used throughout the application.
+// They can be used to store configuration settings, user preferences, or any other data that needs to be accessed globally.
+
+// In js , we can create variables using the `var`, `let`, or `const` keywords.
+
+// Keywords in programming languages are reserved words that have special meaning in the language. They are used to define the structure and behavior of the code. In JavaScript, keywords include `var`, `let`, `const`, `function`, `if`, `else`, `for`, `while`, and many others.
+
+// var was the original way to declare variables in JavaScript. Because of scope issues, it is now recommended to use let and const instead.
+var username = "Albert";
+
+// let is used to declare variables that can be reassigned later. It has block scope, meaning it is only accessible within the block it is defined in.
+let age = 30;
+
+// const is used to declare variables that cannot be reassigned. It is also block scoped.
+const pi = 3.14;
+
+const role = "distributor";
+let darkMode = false;
+// let variables can be reassigned, but const variables cannot be reassigned.
+age = 31;
+// pi = 3.2;
+
+
+// TASK 2: Explain variable naming rules and conventions in JavaScript.
